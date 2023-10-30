c18e98b502a9e7a0d79e581a9192da5a7bc33710
tom <tom@axisj.com>
2023-10-30 14:01:37 +0900

    remove @axframe/contextmenu

--- components/contextMenu/index.tsx
+++ components/contextMenu/index.tsx
@@ -1,2 +1,6 @@
-export * from "./ContextMenu";
-export * from "./TabGroupMenu";
+export enum TabGroupMenuAction {
+  CLOSE_TAB = "CLOSE_TAB",
+  CLOSE_OTHER_TABS = "CLOSE_OTHER_TABS",
+  CLOSE_TABS_RIGHT = "CLOSE_TABS_RIGHT",
+  REFRESH = "REFRESH",
+}
