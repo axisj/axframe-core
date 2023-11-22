17a52d9b1a0850f4bd16ed590635c5b405d4c20b
tom <tom@axisj.com>
2023-11-22 16:25:22 +0900

    Fix core

--- components/DataGrid.tsx
+++ components/DataGrid.tsx
@@ -66,7 +66,7 @@ const Container = styled.div`
       }
 
       [role="rfdg-scroll-container"] {
-        background: ${theme.body_background};
+        background: ${theme.axfdg_body_odd_bg};
       }
     `;
   }}
