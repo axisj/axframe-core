c042ff0bf7a8d8af6d28c9142a4bdebe9550b5dd
tom <tom@axisj.com>
2023-11-29 16:04:33 +0900

    Fix makeProgram

--- scripts/makeProgram/index.ts
+++ scripts/makeProgram/index.ts
@@ -3,7 +3,6 @@ import * as path from "path";
 import programConfig from "../../../makeProgramConfig";
 import decamelize, { camelCase, exist, mkdir } from "./utils";
 import { getNameNDir } from "./getNameNDir";
-import { saveFileByTmpl } from "./saveFileByTmpl";
 
 function main() {
   const { pagesDir, templateDir, programTypeFile, pageRouteFile, routesFile, serviceMockUpDataFile, programs } =
@@ -54,14 +53,25 @@ function main() {
     const programTypeName = decamelize(p.code).toUpperCase();
 
     // 템플릿 파일로 파일 생성
-    saveFileByTmpl({
-      fileNames,
-      type: p.type,
-      templateDir,
-      targetDir,
-      Pascal_programName,
-      programName,
-    });
+    {
+      fileNames.forEach((fn) => {
+        let data = fs.readFileSync(path.join(templateDir, p.type, fn), { encoding: "utf-8" });
+        let pathToFile = path.join(targetDir, fn);
+
+        const rePType = new RegExp(`\\$${p.type}\\$`, "g");
+        const reUsePType = new RegExp(`use\\$${p.type}\\$`, "g");
+
+        pathToFile = pathToFile.replace(rePType, Pascal_programName);
+        data = data.replace(reUsePType, "use" + Pascal_programName).replace(rePType, camelCase(programName));
+
+        if (exist(pathToFile)) {
+          throw "Failed to create file. The file already exists.";
+        } else {
+          console.log(pathToFile);
+          fs.writeFileSync(pathToFile, data);
+        }
+      });
+    }
 
     programTypeFileNew = programTypeFileNew.replace(
       "/* ##ADD_PROGRAM_TYPE_POSITION## */",
